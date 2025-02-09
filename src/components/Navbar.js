7b54682fbe3d9df21093ba7e78cf9d9070813ce2
pxxptm <ppp.pattamon@gmail.com>
2025-02-10 00:12:40 +0700

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <div className='navigation-menu'>
+        <ol>
+            <li>Blogs</li>
+            <li>About</li>
+            <li>Home</li>
+
+
+        </ol>
+    </div>
+
+  )
+}
+
+export default Navbar
