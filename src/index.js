eb316099821377e75613556fc519ceca43309037
pxxptm <ppp.pattamon@gmail.com>
2025-02-10 00:45:56 +0700

    add basename

--- src/index.js
+++ src/index.js
@@ -1,59 +1,44 @@
-// import React, { useEffect } from "react";
-// import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import { createBrowserRouter, RouterProvider } from "react-router-dom";
-// import Disclaimer from "./pages/Disclaimer";
-// import Home from "./pages/Home";
-// import About from "./pages/About";
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Disclaimer from "./src/pages/Disclaimer";
+import Home from "./src/pages/Home";
+import About from "./src/pages/About";
 
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <Disclaimer />,
-//   },
-//   {
-//     path: "/home",
-//     element: <Home />,
-//   },
-//   {
-//     path: "/about",
-//     element: <About />,
-//   },
-// ]);
-
-// const App = () => {
-//   // useEffect(() => {
-//   //   const hasVisitedBefore = localStorage.getItem("hasVisitedLuvlyFancy");
-
-//   //   if (!hasVisitedBefore) {
-//   //     window.location.replace("/");
-//   //   }
-//   // }, []);
-
-//   return <RouterProvider router={router} />;
-// };
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Disclaimer />,
+    },
+    {
+      path: "/home",
+      element: <Home />,
+    },
+    {
+      path: "/about",
+      element: <About />,
+    },
+  ],
+  { basename: "" }
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => {
+  // useEffect(() => {
+  //   const hasVisitedBefore = localStorage.getItem("hasVisitedLuvlyFancy");
+
+  //   if (!hasVisitedBefore) {
+  //     window.location.replace("/");
+  //   }
+  // }, []);
+
+  return <RouterProvider router={router} />;
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
